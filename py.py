be152ebfebb362cdc7a1c4ac077742c43ca5858a
Fernando Carvalho <126211479+Fernando-CarvalhoJr@users.noreply.github.com>
2024-03-08 21:15:37 -0300

    Add files via upload

--- py.py
+++ py.py
@@ -1,37 +1,39 @@
-    class Professional():
-        def __init__(self, nome, especialidade, CRM):
-            self.nome = nome
-            self.especialidade = especialidade
-            self.CRM = CRM
-
-    class Medico(Professional):
-
-        def agendamento(self):
-            print("Medico " + self.nome + " agendou uma consulta")
-
-        def imprimir_info(self):
-            print('nome do medico: ', self.nome)
-            print('especialidade do medico:', self.especialidade)
-            print('numero do CRM: ', self.CRM)
-
-    profissional = Professional('Fernando', 'Clínico', '654321')
-    profissional.imprimir_info()
-
-
-
-#-----------------------------------------------------------------------------------//------------------------------
 class Professional():
     def __init__(self, nome, especialidade, CRM):
         self.nome = nome
         self.especialidade = especialidade
         self.CRM = CRM
 
+    def imprimir_info(self):
+        print('nome: {} especialidade: {} CRM: {}'.format(self.nome, self.especialidade, self.CRM))
+
 class Medico(Professional):
-    def __init__(self, nome, especialidade, CRM):
-        super().__init__(nome, especialidade, CRM)
 
     def agendamento(self):
-        print("Médico " + self.nome + " agendou uma consulta")
+        print("Medico " + self.nome + " agendou uma consulta")
+        
+    def imprimir_info(self):
+        print('Reescrevendo do metodo filho')
 
-profissional = Medico('Fernando', 'Clínico', '654321')
-profissional.agendamento()
+profissional = Professional('Fernando', 'Clínico', '654321')
+profissional.imprimir_info()
+
+medico = Medico('Carol', 'oftalmo', '123456')
+medico.imprimir_info()
+
+# #-----------------------------------------------------------------------------------//------------------------------
+# class Professional():
+#     def __init__(self, nome, especialidade, CRM):
+#         self.nome = nome
+#         self.especialidade = especialidade
+#         self.CRM = CRM
+
+# class Medico(Professional):
+#     def __init__(self, nome, especialidade, CRM):
+#         super().__init__(nome, especialidade, CRM)
+
+#     def agendamento(self):
+#         print("Médico " + self.nome + " agendou uma consulta")
+
+# profissional = Medico('Fernando', 'Clínico', '654321')
+# profissional.agendamento()
