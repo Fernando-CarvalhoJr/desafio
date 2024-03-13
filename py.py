a909c9bd3d0e50170f5c3533c47707fa78606119
Fernando Carvalho <126211479+Fernando-CarvalhoJr@users.noreply.github.com>
2024-03-12 23:42:14 -0300

    Add files via upload

--- py.py
+++ py.py
@@ -1,29 +1,50 @@
-class Professional():
-    def __init__(self, nome, especialidade, CRM):
+class Pessoa:
+    def __init__(self, nome):
         self.nome = nome
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, especialidade):
+        super().__init__(nome)
         self.especialidade = especialidade
+
+class Paciente(Pessoa):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+class Medico(Funcionario):
+    def __init__(self, nome, especialidade, CRM):
+        super().__init__(nome, especialidade)
         self.CRM = CRM
 
-    def imprimir_info(self):
-        print("""-- Nome: {}\n
--- Especialidade: {}""".format(self.nome, self.especialidade))
+    def consulta(self, paciente, data):
+        print('Dr. {} realizou um agendamento para o paciente {} no dia {}'.format(self.nome, paciente, data))
 
-class Medico(Professional):
+    def exame(self, paciente, tipo_exame):
+        print('Dr. {} realizou uma {} no paciente {}'.format(self.nome, tipo_exame, paciente))
 
-    def agendamento(self):
-        print("Medico " + self.nome + " agendou uma consulta")
-        
-    def imprimir_info(self):
-        print("""\n-- Nome: {} \n
--- Especialidade: {} \n
--- CRM: {}\n""".format(self.nome, self.especialidade, self.CRM))
-        
-class Enfermeiro(Professional)
+    def prescrever_medicamento(self, paciente, remedio):
+        print('Dr. {} prescreveu {} para o paciente {}'.format(self.nome, remedio, paciente))
 
-profissional = Professional('Fernando', 'Clínico', '654321')
-profissional.imprimir_info()
+class Enfermeiro(Funcionario):
+    def __init__(self, nome, especialidade, coren):
+        super().__init__(nome, especialidade)
+        self.coren = coren
 
-medico = Medico('Carol', 'oftalmo', '123456')
-medico.imprimir_info()
-medico.agendamento()
+    def injecao(self, paciente):
+        print('Enf. {} aplicou injeção no paciente {}'.format(self.nome, paciente))
+
+    def consulta(self, paciente, data):
+        print('Enf. {} realizou um agendamento para o paciente {} no dia {}'.format(self.nome, paciente, data))
+
+
+# Exemplo de uso
+medico = Medico("Gabriel", "Ortopedista", "12345")
+enfermeiro = Enfermeiro("Jamily", "Clínica Geral", "54321")
+paciente = Paciente("Fernando")
+
+medico.consulta(paciente.nome, "10/03/2024")
+enfermeiro.injecao(paciente.nome)
+medico.exame(paciente.nome, "Raio X")
+enfermeiro.consulta(paciente.nome, "15/03/2024")
+medico.prescrever_medicamento(paciente.nome, "Dorflex")
 
